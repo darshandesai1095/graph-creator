d3998599d8fb3fc90187cbba7b2b5c15cf00188a
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2022-11-20 01:45:29 +0000

    map input data to chart

--- src/App.js
+++ src/App.js
@@ -6,46 +6,46 @@ import { DataGrid } from '@mui/x-data-grid';
 
 function App() {
 
+  // INPUT DATA -> e.g. csv, pasted, typed, etc
+
   const columns = [
-    { field: 'id',        headerName: 'ID',         width: 90 },
-    { field: 'firstName', headerName: 'First name', width: 100,         editable: true },
-    { field: 'lastName',  headerName: 'Last name',  width: 150,         editable: true },
-    { field: 'age',       headerName: 'Age',        type: 'number',     editable: true,   width: 50, },
-    { field: 'fullName',  headerName: 'Full Name',  description: '...', sortable: false,  width: 160 },
+    { field: 'id',    headerName: 'ID',    width: 80,   editable: false},
+    { field: 'month', headerName: 'Month', width: 100,  editable: true },
+    { field: 'param', headerName: 'Temp',  width: 100,  editable: true },
   ];
 
   const rows = [
-    { id: 1, lastName: 'Snow',      firstName: 'Jon',     age: 35},
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei',  age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime',   age: 45 },
-    { id: 4, lastName: 'Stark',     firstName: 'Arya',    age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys',age: null },
-    { id: 6, lastName: 'Melisandre',firstName: null,      age: 150 },
-    { id: 7, lastName: 'Clifford',  firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances',   firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie',     firstName: 'Harvey',  age: 65 },
+    { id: 0, month: 'Jun', param: 5},
+    { id: 1, month: 'Jul', param: 7},
+    { id: 2, month: 'Aug', param: 20},
   ];
 
-  const [label, setLabel] = useState("label 1")
-  const labels = [label, 'Jul', 'Aug']
+
+  // map INPUT DATA to chart
+
+  const [label, setLabel] = useState(0)
+  const labels = rows.map(row => row[Object.keys(row)[1]])
+
+  const row = { id: 0, month: 'Jun', param: 5}
+  console.log("MONTH: " + Object.keys(row)[1])
+
   const data = [
-    { id: 1, label: 'blue',   data: [5, 6, 20]  },
-    { id: 2, label: 'red',    data: [3, 2, -10] },
-    { id: 3, label: 'orange', data: [5, 12, -7] },
+    { id: 0, label: 'Temp',   data: [5, 7, 20]  },
+
   ]
 
   return (
     <div className="App">
 
-      <Box sx={{ height: 600, width: '80%' }}>
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        pageSize={10}
-        rowsPerPageOptions={[10]}
-        experimentalFeatures={{ newEditingApi: true }}
-      />
-    </Box>
+      <Box sx={{ height: 300, width: '80%' }}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          pageSize={10}
+          rowsPerPageOptions={[10]}
+          experimentalFeatures={{ newEditingApi: true }}
+        />
+      </Box>
 
       <form>
         <label>enter label:
