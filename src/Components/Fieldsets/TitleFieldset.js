6fd95837e182bddcdd37c6e9c25d5e06ed99ba2b
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2022-11-26 17:16:09 +0000

    add fieldset css

--- src/Components/Fieldsets/TitleFieldset.js
+++ src/Components/Fieldsets/TitleFieldset.js
@@ -1,10 +1,29 @@
 import React, { useState } from "react";
+import '../../CSS/Fieldsets.css'
+import Switch from '@mui/material/Switch';
 
 function TitleFieldset() {
 
 
     return (
-        <h2>it works</h2>
+        <div class='fieldset'>
+            <div class='field'>
+                <h2>Title</h2>
+                <Switch defaultChecked />
+            </div>
+            <div class='field'>
+                <p>Title</p> 
+            </div>
+            <div class='field'>
+                <p>Font</p>
+            </div>
+            <div class='field'>
+                <p>Text Style</p>
+            </div>
+            <div class='field'>
+                <p>Position</p>
+            </div>
+        </div>
     )
 }
 
