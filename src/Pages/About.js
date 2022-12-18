4ebdce2312f6a0954d4c4a5651192414154130e8
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2022-12-18 23:22:28 +0000

    update about page

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -2,7 +2,16 @@ import React from 'react'
 
 function About() {
     return (
-        <p>About Page</p>
+        <div className='main-content' style={{marginTop: '-100px'}}>
+            <p>The layout for this website is inspired by&nbsp;
+                <a 
+                    href="https://www.chartblocks.io/"
+                    target="_blank">
+                        ChartBlocks
+                </a>
+            </p>
+        </div>
+
     )
 }
 
